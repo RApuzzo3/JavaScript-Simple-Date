888cda87817a9471641c28226fa05ffa5c2067db
Richard Apuzzo III <rapuzzo@digitalatmospheres.com>
2020-06-29 23:13:59 -0400

    Remove jQuery event listener
    Remove jQuery event listener and replace it with VanillaJS.

--- simple-date.js
+++ simple-date.js
@@ -5,10 +5,9 @@
  * @BuildDate: 04/15/2012
  **/
 
-//Get the current year;
 var date = new Date();         //Create new date object;
 var year = date.getFullYear(); //Get year, and set it to variable year;
 
-$(window).load(function() {    //Append date to a span with class date on window load;
-    $('span.date').append(year);
+window.addEventListener('load', function() { 
+	document.getElementById('date').innerHTML = year;
 });
